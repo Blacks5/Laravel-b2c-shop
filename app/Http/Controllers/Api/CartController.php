3231302cf6fc05b80043efbe19df5a03dfce428c
Blacks5 <wuya520123@vip.qq.com>
2017-07-08 17:15:32 +0800

    增加购物车Api

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,13 +5,36 @@ namespace App\Http\Controllers\Api;
 use App\Goods;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function index(Request $request)
     {
         $goods  =   Goods::findOrFail($request->input('id'));
+        $goods->imgs    =   explode(',',$goods->imgs);
 
-        return response()->json($goods);
+        $user   =   Auth::login(2);
+        if($car=Cart::where('uid',$user)->where('gid',$goods->id)->where('info',$request->input('info'))->first()){
+            $num=$car->number+$request->input('number');
+            $cart=Cart::find($car->id);
+            $cart->number=$num;
+            $cart->total=$num*$car->sale;
+        }else{
+            $cart= new  Cart();
+            $cart->uid=$user;
+            $cart->gid=$goods->id;
+            $cart->name=$goods->name;
+            $cart->img=$goods->imgs[0];
+            $cart->info="";
+            $cart->number=$request->input('number');
+            $cart->sale=$goods->sale;
+            $cart->total=$goods->sale*$request->input('number');
+        }
+
+        if($cart->save()){
+            return response()->json(['status'=>200,'text'=>'增加购物车成功!']);
+        }
+        return response()->json(['status'=>400,'text'=>'增加购物车失败!']);
     }
 }
